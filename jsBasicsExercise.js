e8e15c8acdff08b436ab80ebf47a2a77574508c2
Chandler Clarkson <chandlerclarkson@BCs-MacBook-Pro.local>
2020-10-13 09:55:05 -0500

    Completed Exercise

---
+++ jsBasicsExercise.js
@@ -0,0 +1,29 @@
+console.log(7);
+// NUMBERS, Strings, Booleans, NULL, UNDEFINED
+// +, -, *, /, %
+console.log(7%-4);
+
+console.log('I’M MARY POPPINS, Y\'ALL!');
+console.log("Spider" + "-" + "man");
+console.log("Chandler".length + "Clarkson".length);
+
+console.log("Avengers: Endgame"[16]);
+console.log("Avengers: Endgame"["Avengers: Endgame".length-1]);
+
+console.log("\\");
+
+console.log(5 ** 4 - 9);
+
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+
+// console.log("CAP"[1]);
+// console.log("VISION"[0]);
+// console.log("HAWKEYE"[4]);
+// console.log("NICK FURY"[0]);
+// console.log("GROOT"[0]);
+// console.log("DOCTOR STRANGE"[13]);
+// console.log("THOR"[3]);
+// console.log("STAR-LORD"[0])
+
+console.log("CAP"[2] + "\n" + "VISION"[0] + "\n" + "HAWKEYE"[4] + "\n" + "NICK FURY"[0] + "\n" + "GROOT"[0]+ "\n" + "DOCTOR STRANGE"[13] +"\n" + "THOR"[3] + "\n" + "STAR-LORD"[0]);
